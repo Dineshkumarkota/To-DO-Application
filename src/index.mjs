214cc9796a1cea62fa275b118dac2db3c65ed02a
Dineshkumarkota <143687187+Dineshkumarkota@users.noreply.github.com>
2025-07-12 13:12:13 +0530

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,42 @@
+let textInput = document.getElementById("text");
+let btn = document.getElementById("btn");
+let unordered = document.getElementById("unordered");
+
+let dateInput = document.getElementById("date");
+let taskList = [];
+function AddTask(task) {
+  task = textInput.value;
+  taskList.push(task);
+
+  let taskItem = document.createElement("li");
+
+  unordered.appendChild(taskItem);
+  taskList.map((item) => {
+    taskItem.innerHTML = `<input type="checkbox"/> ${item}  <button id="remove">Remove</button>`;
+  });
+  console.log(taskList);
+  let removeBtn = document.getElementById("remove");
+  removeBtn.addEventListener("click", removeTask);
+
+  textInput.value = "";
+
+  event.preventDefault();
+}
+btn.addEventListener("click", AddTask);
+
+function deadline() {
+  let deadline = dateInput.value;
+  let deadlineDate = new Date(deadline);
+  let deadlineFuture = deadlineDate.getDate();
+  let date = new Date();
+  let dateNow = date.getDate();
+  if (deadlineFuture < dateNow) {
+    alert("Please use future date");
+  }
+}
+
+btn.addEventListener("click", deadline);
+
+function removeTask(task) {
+  taskList.split(task, 1);
+}
